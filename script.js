1dcbe79b208632fb416172347825bbd2792ba24c
ppr350 <kobito610@gmail.com>
2023-09-25 12:16:26 +0100

    Testing with adding book to page

--- script.js
+++ script.js
@@ -6,18 +6,26 @@ function Book(title, author, pages, readIt) {
 	this.pages = pages,
 	this.readIt = readIt
 	this.description = function() {
-		console.log(`${this.title} is written by ${this.author} and it has ${this.pages}.`)
+		console.log(`${this.title} is written by ${this.author} and it has ${this.pages} pages.`)
 		if (this.readIt === true) {
 			console.log('I\'ve read it.')
 		} else {
 			console.log('I haven\'t read it yet.')
 		}
 	}
-	const addBookToShelf = bookShelf.push(this.title, this.author, this.pages, this.readIt);
+	const addBookToShelf = bookShelf.push(this.title);
 	return;
 }
 
-const pythonCrashCourse = new Book("Pyhton Crash Course", "Eric Matthes", "552", true)
+
+// books //
+const pythonCrashCourse = new Book("Pyhton Crash Course", "Eric Matthes", "552", true);
+
+const motherfocloir = new Book("MotherFocloir", "Darach O Seaghdha", "222", true);
+
+const thinkLikeAProgrammer = new Book("Think Like A Programmer", "V. Anton Spraul", true);
+
+const IrishMythsAndLegends = new Book("Irish Myths And Legend", "Lady Gregory", "456", false);
 
 function addBookToShelf() {
 
@@ -26,14 +34,28 @@ function addBookToShelf() {
 const changeStatus = {
 	flipReadUnRead: function () {
 		console.log("Read it or not")
+		console.log(this.readIt)
 		if (this.readIt === true) {
-			readIt === false;
+			this.readIt === false;
+			console.log(this.description);
 		} else if (this.readIt === false) {
-			readIt === true;
+			this.readIt === true;
+			console.log(this.description);
 		}
-		console.log(readIt);
+		console.log(`${this.description}`);
 
 	}
 }
 
-const read = changeStatus.flipReadUnRead();
+const read = changeStatus.flipReadUnRead(pythonCrashCourse);
+
+// let bookDisplay = document.getElementById("book-display")
+
+function stackTheBookDisplay() {
+	// let bookDisplay = document.getElementById("book-display")
+	for (i = 0; i < bookShelf; i++) {
+		document.getElementById("book-display").innerText = array[i];
+	} 
+}
+
+stackTheBookDisplay()
