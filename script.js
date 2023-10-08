703c09b8f572d51f2a37d20321ce0717e51fadc1
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2023-10-08 10:10:46 +0100

    finding a way to make local variable works on those global functions

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function Book(title, author, pages, readIt) {
 
 // Constructor invocation to change READING STATUS //
 Book.prototype.changeReadingStatus = function() {
-	debugger
+
 	this.readIt = !this.readIt;
 	const bookTitleRegExed = this.title.replace(/\s/g, "");
 	String(bookTitleRegExed);
@@ -34,7 +34,6 @@ Book.prototype.changeReadingStatus = function() {
 	}
 }
 
-// Constructor invocation to display the book on the page //
 Book.prototype.stackTheBookDisplay = function() {
 
 	// Create a div for THIS BOOK and be adopted by bookDisplay //
@@ -71,19 +70,23 @@ Book.prototype.stackTheBookDisplay = function() {
 	thisBook.appendChild(bookReadingStatus);
 }
 
+// Constructor invocation to display the book on the page //
+
 // Constructor invocation with "new" keyword to add BOOKS to the Book function above //
-const pythonCrashCourse = new Book("Python Crash Course", "Eric Matthes", "552", true);
+// const pythonCrashCourse = new Book("Python Crash Course", "Eric Matthes", "552", true);
 
-const motherfocloir = new Book("MotherFocloir", "Darach O Seaghdha", "222", true);
+// const motherfocloir = new Book("MotherFocloir", "Darach O Seaghdha", "222", true);
 
-const thinkLikeAProgrammer = new Book("Think Like A Programmer", "V. Anton Spraul", true);
+// const thinkLikeAProgrammer = new Book("Think Like A Programmer", "V. Anton Spraul", true);
 
 const irishMythsAndLegends = new Book("Irish Myths And Legend", "Lady Gregory", "456", false);
+console.log(irishMythsAndLegends);
 
 // Test call //
 // pythonCrashCourse.stackTheBookDisplay();
 // pythonCrashCourse.changeReadingStatus();
 irishMythsAndLegends.stackTheBookDisplay();
+irishMythsAndLegends.description();
 
 const form = document.querySelector('form');
 const dialog = document.querySelector('#add-book-dialog')
@@ -125,19 +128,78 @@ function getUserInputs() {
 	const authorValue = document.querySelector('#input-author').value;
 	const pagesValue = document.querySelector('#input-pages').value;
 	// const statusValue = document.querySelector('#input-read-it').value;
-
+	// debugger
 	let getStatus = document.querySelector('#input-read-it');
 	let statusValue;
-	if (getStatus.value === 'yes') {
+	if (getStatus.value === 'true') {
 		statusValue = true;
-	} else if (getStatus.value === 'no') {
+	} else if (getStatus.value === 'false') {
 		statusValue = false;
 	};
+
+	// function Book(title, author, pages, readIt) {
+	// 	this.title = title,
+	// 	this.author = author,
+	// 	this.pages = pages,
+	// 	this.readIt = Boolean(readIt),
+	// 	this.description = function() {
+	// 		console.log(`${this.title} is written by ${this.author} and it has ${this.pages} pages.`)
+	// 		if (this.readIt === true) {
+	// 			console.log('I\'ve read it.')
+	// 		} else {
+	// 			console.log('I haven\'t read it yet.')
+	// 		}
+	// 	}
+	// 	return;
+	// }
+
+	// Book.prototype.stackTheBookDisplay = function() {
+
+	// 	// Create a div for THIS BOOK and be adopted by bookDisplay //
+	// 	const thisBook = document.createElement('div');
+	// 	thisBook.setAttribute("id", this.title.replace(/\s/g, ""));
+	// 	bookDisplay.appendChild(thisBook);
+	
+	// 	// Add BOOK TITLE to page //
+	// 	const bookTitle = document.createElement('p');
+	// 	bookTitle.classList.add('book-title');
+	// 	bookTitle.dataset.title = this.title;
+	// 	bookTitle.innerText = this.title;
+	// 	thisBook.appendChild(bookTitle);
+	
+	// 	// Add BOOK AUTHOR to page //
+	// 	const bookAuthor = document.createElement('p');
+	// 	bookAuthor.classList.add('book-author');
+	// 	bookAuthor.dataset.author = this.author;
+	// 	bookAuthor.innerText = this.author;
+	// 	thisBook.appendChild(bookAuthor);
+	
+	// 	// Add BOOK PAGES to page //
+	// 	const bookPages = document.createElement('p');
+	// 	bookPages.classList.add('book-pages');
+	// 	bookPages.dataset.pages = this.pages;
+	// 	bookPages.innerText = this.pages;
+	// 	thisBook.appendChild(bookPages);
+	
+	// 	// Add BOOK READING STATUS to page //
+	// 	const bookReadingStatus = document.createElement('p');
+	// 	bookReadingStatus.classList.add('book-status');
+	// 	bookReadingStatus.innerText = this.readIt === true ? "YES" : "NO";
+	// 	bookReadingStatus.dataset.status = this.readIt === true ? "YES" : "NO";
+	// 	thisBook.appendChild(bookReadingStatus);
+	// }
+	
+
 	// const statusValue = getStatus.value;
 	// console.log(statusValue);
 	let addNewBook = new Book(titleValue, authorValue, pagesValue, statusValue);
+	console.log(addNewBook)
 	addNewBook.stackTheBookDisplay();
-	debugger
-	Object.setPrototypeOf(this.addNewBook, Book.prototype);
+	addNewBook.description();
+
+	// const motherfocloir = new Book("MotherFocloir", "Darach O Seaghdha", "222", true);
+	// console.log(motherfocloir);
+	// motherfocloir.stackTheBookDisplay();
+	// motherfocloir.description();
 }
 
