c7b90113e3a2754ba9a4d36884c518d8045308db
ppr350 <kobito610@gmail.com>
2023-10-15 16:27:46 +0100

    changeReadingStatus() prototype works on stackTheBookDisplay() prototype function now. The solution is arrow function, it lets the inner code to have access to the outer scope

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const bookDisplay = document.getElementById("book-display");
 
 // Constructor function to add NEW BOOKS w//
 function Book(title, author, pages, read) {
+
 	this.title = title,
 	this.author = author,
 	this.pages = pages,
@@ -19,6 +20,7 @@ function Book(title, author, pages, read) {
 
 // Prototype function of Book to change READING STATUS //
 Book.prototype.changeReadingStatus = function() {
+
 	this.status = !this.status;
 	const bookTitleRegExed = this.title.replace(/\s/g, "");
 	// String(bookTitleRegExed);
@@ -38,6 +40,7 @@ let newBook = {};
 
 // Prototype function of Book to be get new Book info from user //
 Book.prototype.add = function() {
+
 	const title = document.querySelector('#input-title').value;
 	const author = document.querySelector('#input-author').value;
 	const pages = document.querySelector('#input-pages').value;
@@ -89,54 +92,37 @@ Book.prototype.stackTheBookDisplay = function() {
 	statusButton.setAttribute('id', this.title.replace(/\s/g, "-") + '-status-button')
 	statusButton.innerText = 'CHANGE STATUS';
 	thisBook.appendChild(statusButton);
-	statusButton.addEventListener('click', function() {
-		console.log('change status')
-		console.log(thisBook.innerText)
-		let thisBookStatus = thisBook.querySelector('.book-status')
-		console.log(thisBookStatus)
-		// thisBook.bookReadingStatusinnerText = this.status === true ? "NO" : "YES";
-		// bookReadingStatus.dataset.status = this.status === true ? "NO" : "YES";
-		// thisBook.changeReadingStatus();
-	})
+	statusButton.addEventListener('click', () => {
+		this.changeReadingStatus();
+	});
 
 	// Add a button to remove BOOK //
 	const removeButton = document.createElement('button');
 	removeButton.classList.add('remove-button', 'button');
 	removeButton.innerText = 'REMOVE';
 	thisBook.appendChild(removeButton);
-	removeButton.addEventListener('click', function() {
+	removeButton.addEventListener('click', () => {
 		thisBook.remove();
-	})
-}
+	});
+};
 
-
-// Constructor invocation with "new" keyword to add BOOKS to the Book function above //
-// const pythonCrashCourse = new Book("Python Crash Course", "Eric Matthes", "552", true);
-
-// const motherfocloir = new Book("MotherFocloir", "Darach O Seaghdha", "222", true);
-
-// const thinkLikeAProgrammer = new Book("Think Like A Programmer", "V. Anton Spraul", true);
-
+// Test run //
 const irishMythsAndLegends = new Book("Irish Myths And Legend", "Lady Gregory", "456", false);
-console.log(irishMythsAndLegends);
-console.log(Object.getPrototypeOf(irishMythsAndLegends));
-
-// Test call //
-// pythonCrashCourse.stackTheBookDisplay();
-// pythonCrashCourse.changeReadingStatus();
 irishMythsAndLegends.stackTheBookDisplay();
-// irishMythsAndLegends.description();
 
+// INPUT DIALOG //
 const form = document.querySelector('form');
 const dialog = document.querySelector('#add-book-dialog')
 const showDialog = document.querySelector('#show-dialog-button');
 const addBookThenCloseDialog = document.querySelector('#add-now-button');
 const cancelThenCloseDialog = document.querySelector('#cancel-button');
 
+// Add button to show INPUT DIALOG //
 showDialog.addEventListener('click', () => {
 	dialog.showModal();
-})
+});
 
+// Add functionality to INPUT DIALOG //
 form.addEventListener('submit', (e) => {
 	// Prevent the default behavior of submitting form //
 	e.preventDefault();
@@ -144,15 +130,13 @@ form.addEventListener('submit', (e) => {
 	Book.prototype.add();
 	// Call this prototype function to add the Book instance to the display //
 	newBook.stackTheBookDisplay();
-	console.log(newBook);
-	console.log(Object.getPrototypeOf(newBook));
 	dialog.close();
 });
 
+// Add button to close and cancel INPUT DIALOG //
 cancelThenCloseDialog.addEventListener('click', (e) => {
 	// Prevent the default behavior of submitting form //
 	e.preventDefault()
-	console.log("click CANCEL, and script to preventDefault")
 	dialog.close();
 });
 
